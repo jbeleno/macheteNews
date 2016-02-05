c573dc8bb880c757662ae948122c0b0ced0c9a43
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2016-02-04 23:28:35 -0500

    app.js modified to get access to node app in a port

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+    server();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
