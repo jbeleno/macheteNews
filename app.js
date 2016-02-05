3de9166ffda4df8904ae2edda7cc030a3279f684
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2016-02-04 23:41:14 -0500

    removing the server method that didn't exist to see what happen

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
     console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
-    server();
+    //server();
 });
 
 // view engine setup
