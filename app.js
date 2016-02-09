bd7d2e1838c895bc67c93ca8e3127dbf735a1dd6
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2016-02-08 20:17:35 -0500

    Some minor changes in the code to improve the good practices

--- app.js
+++ app.js
@@ -18,16 +18,6 @@ var scrape = require('./routes/scrape');
 var users = require('./routes/users');
 
 var app = express();
-var http = require('http');
-
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
-
-
-http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
-    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
-    //server();
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
